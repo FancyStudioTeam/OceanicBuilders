1a94944c427e5189b5bc6f0299dc385759bf66be
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-19 19:27:25 +0200

    Add `types` and update `.gitignore`

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+import type {
+  ButtonComponent,
+  EmbedField as OceanicEmbedField,
+  PollMedia as OceanicPollMedia,
+  SelectMenuComponent,
+  SelectOption,
+} from "oceanic.js";
+import type {
+  Button,
+  ChannelSelectMenu,
+  EmbedField,
+  MentionableSelectMenu,
+  PollMedia,
+  RoleSelectMenu,
+  StringSelectMenu,
+  StringSelectMenuOption,
+  UserSelectMenu,
+} from ".";
+
+export type BuilderComponents =
+  | Button
+  | ChannelSelectMenu
+  | MentionableSelectMenu
+  | RoleSelectMenu
+  | StringSelectMenu
+  | UserSelectMenu;
+
+export type JSONComponents = ButtonComponent | SelectMenuComponent;
+
+export type ValidComponents = BuilderComponents | JSONComponents;
+
+export type ValidEmbedField = EmbedField | OceanicEmbedField;
+
+export type ValidPollMedia = PollMedia | OceanicPollMedia;
+
+export type ValidSelectMenuOption = StringSelectMenuOption | SelectOption;
