b4ca0e34c6566662651aa796edc453179bd781de
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2024-12-01 13:52:23 +0100

    Move buttons tests inside buttons folder

---
+++ src/builders/buttons/__tests__/PrimaryButtonBuilder.test.ts
@@ -0,0 +1,114 @@
+import { ButtonStyles, ComponentTypes, type TextButton } from "oceanic.js";
+import { describe, expect, it } from "vitest";
+import { PrimaryButtonBuilder } from "../PrimaryButtonBuilder.js";
+
+const PrimaryButton = (data?: Partial<TextButton>) => new PrimaryButtonBuilder(data);
+
+describe("PrimaryButtonBuilder", () => {
+  describe("Using builder methods", () => {
+    it("Should return JSON base", () =>
+      expect(PrimaryButton().toJSON()).toStrictEqual({
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with custom ID", () =>
+      expect(PrimaryButton().setCustomID("test").toJSON()).toStrictEqual({
+        customID: "test",
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with disabled option enabled", () =>
+      expect(PrimaryButton().setDisabled().toJSON()).toStrictEqual({
+        disabled: true,
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with custom label", () =>
+      expect(PrimaryButton().setLabel("Test").toJSON()).toStrictEqual({
+        label: "Test",
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with custom emoji", () =>
+      expect(
+        PrimaryButton()
+          .setEmoji({
+            name: "🤖",
+          })
+          .toJSON(),
+      ).toStrictEqual({
+        emoji: {
+          name: "🤖",
+        },
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with cleared label", () =>
+      expect(PrimaryButton().setLabel("Test").clear("label").toJSON()).toStrictEqual({
+        label: undefined,
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with force cleared label", () =>
+      expect(PrimaryButton().setLabel("Test").clear("label", true).toJSON()).toStrictEqual({
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with cleared emoji", () =>
+      expect(
+        PrimaryButton()
+          .setEmoji({
+            name: "🤖",
+          })
+          .clear("emoji")
+          .toJSON(),
+      ).toStrictEqual({
+        emoji: undefined,
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Should return JSON base with force cleared emoji", () =>
+      expect(
+        PrimaryButton()
+          .setEmoji({
+            name: "🤖",
+          })
+          .clear("emoji", true)
+          .toJSON(),
+      ).toStrictEqual({
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+  });
+
+  describe("Using external data", () => {
+    it("Receive secondary button style but should return primary button style", () =>
+      expect(
+        PrimaryButton({
+          style: ButtonStyles.SECONDARY,
+        }).toJSON(),
+      ).toStrictEqual({
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+
+    it("Receive action row type but should return button type", () =>
+      expect(
+        PrimaryButton({
+          // @ts-expect-error
+          type: ComponentTypes.ACTION_ROW,
+        }).toJSON(),
+      ).toStrictEqual({
+        style: ButtonStyles.PRIMARY,
+        type: ComponentTypes.BUTTON,
+      }));
+  });
+});
