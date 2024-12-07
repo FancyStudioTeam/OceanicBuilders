3bec3d3b58aa9da90334d5968b533dbee47195d6
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2024-12-07 14:53:59 +0100

    More changes

--- esm.mjs
+++ esm.mjs
@@ -1,12 +1,1 @@
-export const { ActionRow } = await import("./dist/builders/ActionRow.js");
-export const { Attachment } = await import("./dist/builders/Attachment.js");
-export const { Button } = await import("./dist/builders/Button.js");
-export const { ChannelSelectMenu } = await import("./dist/builders/ChannelSelectMenu.js");
-export const { Embed } = await import("./dist/builders/Embed.js");
-export const { MentionableSelectMenu } = await import("./dist/builders/MentionableSelectMenu.js");
-export const { Modal } = await import("./dist/builders/Modal.js");
-export const { Poll, PollMedia } = await import("./dist/builders/Poll.js");
-export const { RoleSelectMenu } = await import("./dist/builders/RoleSelectMenu.js");
-export const { StringSelectMenu, StringSelectMenuOption } = await import("./dist/builders/StringSelectMenu.js");
-export const { TextInput } = await import("./dist/builders/TextInput.js");
-export const { UserSelectMenu } = await import("./dist/builders/UserSelectMenu.js");
+export * from "./dist/builders/index.js";
