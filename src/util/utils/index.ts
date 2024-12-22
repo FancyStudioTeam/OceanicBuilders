4ab13208a92075451cc591873e7657d999633de3
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2024-12-22 13:19:28 +0100

    Add `normalizeArray` function

--- src/util/utils/index.ts
+++ src/util/utils/index.ts
@@ -1,3 +1,4 @@
+import { type RestOrArray, normalizeArray } from "./normalizeArray.js";
 import { parseEmoji } from "./parseEmoji.js";
 
-export { parseEmoji };
+export { normalizeArray, parseEmoji, type RestOrArray };
